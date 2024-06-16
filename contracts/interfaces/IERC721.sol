4d7b735c621a62a03a8c1aaa7adc9dd8c79c6fe2
dapanpan <njdpp01@163.com>
2024-06-16 09:44:58 +0800

    add nft contracts

---
+++ contracts/interfaces/IERC721.sol
@@ -0,0 +1,21 @@
+/*
+    
+   ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+  ██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+  ██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+  ██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+   ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+  
+  Find any smart contract, and build your project faster: https://www.cookbook.dev
+  Twitter: https://twitter.com/cookbook_dev
+  Discord: https://discord.gg/cookbookdev
+  
+  Find this contract on Cookbook: https://www.cookbook.dev/contracts/OpenZeppelin-ERC721?utm=code
+  */
+  
+  // SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (interfaces/IERC721.sol)
+
+pragma solidity ^0.8.20;
+
+import {IERC721} from "../token/ERC721/IERC721.sol";
